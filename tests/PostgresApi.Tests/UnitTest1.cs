ca74ce52290d710ac396d62f6c0ca21b9b9e75d2
Daniel Salter <dan.salter@finbourne.com>
2024-10-13 18:13:08 +0200

    add webapi PostgresApi and nunit PostgresApi.Tests

---
+++ tests/PostgresApi.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace PostgresApi.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
